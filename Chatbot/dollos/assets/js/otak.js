90d7af26a35ba4bd769b09822ca17656091ce745
D3NCyann <75367447+D3NCyann@users.noreply.github.com>
2021-03-15 09:50:14 +0800

    Update otak.js

--- Chatbot/dollos/assets/js/otak.js
+++ Chatbot/dollos/assets/js/otak.js
@@ -13,38 +13,66 @@
 // perintah user 
 const prompts = [
   // pertanyaan formal
-  ["hi", "hai", "halo", "woi", "euy", "oi", "hey", "hey kamu"],
-  ["apa kabar", "apa kabar hari ini"],
-  ["siapa namamu", "siapa nama mu", "siapa kamu", "siapa kau", "kamu siapa"],
+  ["hi", "hai", "hai kamu", "halo", "woi", "euy", "oi", "hey", "hey kamu"],
+  ["apa kabar", "apa kabar hari ini", "kabarnya gimana"],
+  ["lagi apa", "lagi apa ni", "lagi apa kamu", "ngapain", "lagi ngapain"],
+  ["siapa namamu", "siapa nama mu", "siapa kamu", "siapa kau", "siapa ini", "dengan siapa ini", "kamu siapa"],
+  ["selamat pagi", "pagi"],
+  ["selamat malam", "malam"],
+  ["siapa pencipta mu", "siapa yang menciptakan mu", "siapa yang membuat mu"],
+
+
+  // bucin
+  ["tau apa", "gatau", "engga", "ga"],
+  ["ajarin apa", "ajar apa"],
 
 
   // pertanyaan advanced
-  ["_jam", "jam berapa sekarang", "sekarang jam berapa", "jam berapa ini", "jam berapa", "sekarang jam"]
+  ["_help"],
+  ["_jam", "jam berapa sekarang", "sekarang jam berapa", "jam berapa ini", "jam berapa", "sekarang jam"],
+  ["_bucin"]
 ]
 
 // jawaban bot
 const replies = [
   // jawaban untuk pertanyaan formal
-  ["Halo", "Iya", "Ada yang bisa saya bantu?"],
+  ["Halo", "Iya", "Hai.. ada yang bisa saya bantu?"],
   [
     "Wo jelas baik.. bagaimana dengan mu",
     "Tentu baik, kamu?",
     "Baik.. aku mau nanya nih"
   ],
-  ["Namaku DollosChat"],
+  ["Biasalaaah", "lagi memikirkan hidup kedepannya",],
+  ["Namaku DollosChat", "DollosChat", "DollosChat biasa disingkat DC", "Chatbot DC"],
+  ["Selamat pagi", "pagi juga", "pagi juga kamunya", "pagi :D"],
+  ["Selamat malam", "malam juga", "malam juga kamunya", "malam :)"],
+  ["Saya diciptakan dengan kasih sayang", "Mereka banyak", "Orang orang hebat"],
+
+
+  // bucin
+  ["tau ga sebelah tulang rusuk ku ada di kamu... avv"],
+  ["ajarin aku cara dapetin kamu.. avv"],
 
 
   // jawaban pertanyaan advanced
-  [hoursNow()]
+  [
+    `Ini adalah beberapa kumpulan shorcut yang bisa kamu gunakan
+    _help untuk kumpulan shorcut
+    _jam untuk lihat jam sekarang
+    _bucin untuk percakapan bucin`
+  ],
+  [hoursNow()],
+  ["tau ga?", "yaudah ajarin aku aja"]
 ]
 
 // text random kalau tidak sesuai
 const alternative = [
-  "Apa?",
-  "Tolong gunakan bahasa yang baik dan benar yahh",
-  "Tau ga?",
-  "Coba lagi.. saya kurang paham",
-  "Saya masih belajar tolong bersabar",
+  "Jadi ngantuk",
+  "Gini amat hidup",
+  "Percakapan absurd macam apa ini",
+  "Coba lagi.. mungkin ada typo",
+  "Saya masih belajar sih.. ajar dong",
+  "Ya sudah biarkan saya cerita",
   "Saya tidak mengerti.. :("
 ]
 
