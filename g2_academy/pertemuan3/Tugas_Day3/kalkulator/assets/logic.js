9dbca6ef180a7fab7b4f49505fffafa693a87324
Doltons Edward Nicholas <75367447+D3NCyann@users.noreply.github.com>
2021-07-21 16:03:10 +0800

    Delete g2_academy/pertemuan3/Tugas_Day3 directory

--- g2_academy/pertemuan3/Tugas_Day3/kalkulator/assets/logic.js
+++
@@ -1,9 +0,0 @@
-let autoFill = (target)=> document.calc.output.value += target,
-    equal = ()=> {
-        let valEqual = document.getElementById('valueEqual');
-        valEqual.innerHTML = eval(calc.output.value);
-    }
-
-function del() {
-    document.calc.output.value = document.calc.output.value.slice(0, -1);
-}
